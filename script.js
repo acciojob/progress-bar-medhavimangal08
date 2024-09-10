6270bf43019bbfde380147b0f722b2ed556b2e63
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-10 20:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+const circles = document.querySelectorAll('.circle');
+const btn=document.querySelector("button")
+const nextbtn=document.querySelector("#next");
+const prevbtn=document.querySelector("#prev");
+
+prevbtn.disabled=true;
+let classactive=0;
+btn.addEventListener("click", classmodify)
+
+function classmodify(e) {
+	if(e.target==nextbtn){
+		classactive++
+		circles[classactive].classList.add("active")
+                prevbtn.disabled=false;
+
+	}
+	if(classactive==circles.length-1){
+		nextbtn.disabled=true;
+	}
+	if(e.target==prevbtn){
+		classactive--
+		circles[classactive+1].classList.remove("active")
+	}
+	if(classactive==0){
+		prevbtn.disabled=true
+	}
+	
+}
+
